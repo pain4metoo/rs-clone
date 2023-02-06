b162eeea23f0338c820ee50f10f05952b82c21be
pain4metoo <pain4metoo@gmail.com>
2023-02-06 20:33:32 +0100

    refactor: data-controller add try catch contruction

--- src/ts/api/data-controller.ts
+++ src/ts/api/data-controller.ts
@@ -3,27 +3,67 @@ import { baseUrl, path } from './routes';
 import { CategoryData } from './types';
 
 export class DataController {
-  public static async getCategories(): Promise<Array<CategoryData>> {
-    const response: Response = await fetch(`${baseUrl}${path.categories}`);
-    const categories: Array<CategoryData> = await response.json();
-    return categories;
+  public static async getCategories(): Promise<Array<CategoryData> | void> {
+    try {
+      const response: Response = await fetch(`${baseUrl}${path.categories}`);
+
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+      
+      const categories: Array<CategoryData> = await response.json();
+
+      return categories;
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-  public static async getLesson(id: number): Promise<LessonData> {
-    const response: Response = await fetch(`${baseUrl}${path.lessons}/${id}`);
-    const lesson: LessonData = await response.json();
-    return lesson;
+  public static async getLesson(id: number): Promise<LessonData | void> {
+    try {
+      const response: Response = await fetch(`${baseUrl}${path.lessons}/${id}`);
+
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+
+      const lesson: LessonData = await response.json();
+
+      return lesson;
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-  public static async getTest(id: number): Promise<TestData> {
-    const response: Response = await fetch(`${baseUrl}${path.tests}/${id}`);
-    const test: TestData = await response.json();
-    return test;
+  public static async getTest(id: number): Promise<TestData | void> {
+    try {
+      const response: Response = await fetch(`${baseUrl}${path.tests}/${id}`);
+
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+
+      const test: TestData = await response.json();
+
+      return test;
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-  public static async getTask(id: number): Promise<TaskData> {
-    const response: Response = await fetch(`${baseUrl}${path.tasks}/${id}`);
-    const task: TaskData = await response.json();
-    return task;
+  public static async getTask(id: number): Promise<TaskData | void> {
+    try {
+      const response: Response = await fetch(`${baseUrl}${path.tasks}/${id}`);
+
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+
+      const task: TaskData = await response.json();
+
+      return task;
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
