ecec18de5ce5854451e60f211339c1ec93fa2eab
Evgeniya Ermishina <evermishka@yandex.ru>
2023-02-09 15:10:03 +0300

    feat: add done items render

--- src/ts/components/main/categories-page/categories-page.ts
+++ src/ts/components/main/categories-page/categories-page.ts
@@ -1,7 +1,14 @@
 import { DataController } from '../../../api/data-controller';
 import Control from '../../../common/control';
 import { state } from '../../../common/state';
-import { ArticleMetaData, CategoriesType, CategoryContent, LessonData, TaskData, TestData } from '../../../common/state-types';
+import {
+  ArticleMetaData,
+  CategoriesType,
+  CategoryContent,
+  LessonData,
+  TaskData,
+  TestData,
+} from '../../../common/state-types';
 import { PagesList } from '../main';
 
 export class CategoriesPage extends Control {
@@ -52,7 +59,19 @@ export class CategoriesPage extends Control {
       accordionCollapse.node.setAttribute('data-bs-parent', `#accordionExample`);
       const accordionBody = new Control(accordionCollapse.node, 'div', 'accordion-body d-flex flex-column');
       category.items.forEach((item) => {
-        const categoryItem: Control<HTMLLinkElement> = new Control(accordionBody.node, 'a', '', item.name);
+        const categoryItem: Control<HTMLLinkElement> = new Control(
+          accordionBody.node,
+          'a',
+          'link-primary d-flex align-items-center',
+          item.name
+        );
+        if (isAuth && this.isItemDone(item, type)) {
+          categoryItem.node.classList.add('link-secondary');
+          categoryItem.node.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle ms-1" viewBox="0 0 16 16">
+          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+          <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+        </svg>`;
+        }
         categoryItem.node.href = '#';
         categoryItem.node.onclick = (): Promise<void> => this.switchPage(type, item.id);
       });
@@ -82,17 +101,23 @@ export class CategoriesPage extends Control {
     }
   }
 
-  private getProgressValue(items: Array<ArticleMetaData>, type: keyof CategoriesType,): number {
+  private getProgressValue(items: Array<ArticleMetaData>, type: keyof CategoriesType): number {
     const allItemsCount = items.length;
     const doneItems = state.getUser().done[type] as Array<ArticleMetaData>;
     let doneItemsCount = 0;
     items.forEach((item) => {
       doneItems.forEach((doneItem) => {
         if (item.id === +doneItem.id) {
-          doneItemsCount +=1;
+          doneItemsCount += 1;
         }
-      })
+      });
     });
-    return doneItemsCount ? Math.round((doneItemsCount / allItemsCount) * 100) : 0; 
+    return doneItemsCount ? Math.round((doneItemsCount / allItemsCount) * 100) : 0;
+  }
+
+  private isItemDone(item: ArticleMetaData, type: keyof CategoriesType): boolean {
+    const doneItems = state.getUser().done[type] as Array<ArticleMetaData>;
+    const result = doneItems.find((doneItem) => item.id === +doneItem.id);
+    return result ? true : false;
   }
 }
