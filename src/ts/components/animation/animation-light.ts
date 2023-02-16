b70bf5dbdd9ba5c099ef312a4a5ed7c35356a42d
pain4metoo <pain4metoo@gmail.com>
2023-02-16 15:05:16 +0100

    feat: add light bg animation

--- src/ts/components/animation/animation-light.ts
+++ src/ts/components/animation/animation-light.ts
@@ -3,11 +3,6 @@ import './animation-light.scss';
 
 export class AnimationLight extends Control {
   constructor(parentNode: HTMLElement) {
-    super(parentNode, 'div', 'light');
-
-    const rayCount: number = 10;
-    for (let i = 1; i < rayCount; i++) {
-      new Control(this.node, 'div', `x${i}`);
-    }
+    super(parentNode, 'div', 'bg-animation');
   }
 }
